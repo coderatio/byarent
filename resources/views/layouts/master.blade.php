f28466914ce57d2be888108cb8f0d07e8a387ee0
Coderatio <dmcbist@gmail.com>
2019-02-24 14:46:25 +0100

    Modified some files

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,13 +10,13 @@
 
     <!-- CSS
     ================================================== -->
-    <link rel="stylesheet" href="/public/css/app.css">
-    <link rel="stylesheet" href="/public/vendor/animate/animate.css">
-    <link rel="stylesheet" href="/public/vendor/template/css/style.css">
-    <link rel="stylesheet" href="/public/vendor/sweetalert/sweetalert.css">
-    <link rel="stylesheet" href="/public/vendor/uikit/css/uikit.min.css">
-    <link rel="stylesheet" href="/public/vendor/template/css/colors/red.css" id="colors">
-    <link rel="stylesheet" href="public/css/custom.css" id="colors">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/animate/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/template/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/sweetalert/sweetalert.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/uikit/css/uikit.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/template/css/colors/red.css') }}" id="colors">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}" id="colors">
 
 </head>
 

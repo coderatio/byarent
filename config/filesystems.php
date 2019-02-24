78aef2b958caf8d0ae6247c66278af7189d33e2e
Coderatio <dmcbist@gmail.com>
2019-02-24 18:41:19 +0100

    Made few changes to filesystems.php config file

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,10 +48,16 @@ return [
             'root' => storage_path('app'),
         ],
 
-        'public' => [
+        /*'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],*/
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage/app/public',
             'visibility' => 'public',
         ],
 

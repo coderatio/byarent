8198c8d45cda332ad1a703fdcfb764a605a61500
Coderatio <dmcbist@gmail.com>
2019-02-24 14:20:56 +0100

    Modified master file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale">
+<html lang="{{ config('app.locale') }}">
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <!-- Basic Page Needs

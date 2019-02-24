a6b0f37bd5f117b46ae71435559eaf3c777b0f90
Coderatio <dmcbist@gmail.com>
2019-02-24 14:47:08 +0100

    Modified some files

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,5 +19,5 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
